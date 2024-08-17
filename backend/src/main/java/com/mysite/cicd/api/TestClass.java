13f3a50e4272351e00e7d387a13a1245c28e4088
Yeong-Huns <156881254+Yeong-Huns@users.noreply.github.com>
2024-08-18 08:35:58 +0900

    second test

---
+++ backend/src/main/java/com/mysite/cicd/api/TestClass.java
@@ -0,0 +1,15 @@
+package com.mysite.cicd.api;
+
+/**
+ * packageName    : com.mysite.cicd.api
+ * fileName       : TestClass
+ * author         : Yeong-Huns
+ * date           : 2024-08-18
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-08-18        Yeong-Huns       최초 생성
+ */
+public class TestClass {
+}
